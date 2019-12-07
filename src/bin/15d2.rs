6c0fb1498abf076fba2f1dcd1bf8a783cf0025f3
crumblingstatue <radiantstatue@gmail.com>
2019-12-07 22:51:02 +0100

    Solve 2015 day 2

---
+++ src/bin/15d2.rs
@@ -0,0 +1,60 @@
+fn parse_lwh(text: &str) -> (i32, i32, i32) {
+    let mut splits = text.split('x');
+    let l = splits.next().unwrap().parse().unwrap();
+    let w = splits.next().unwrap().parse().unwrap();
+    let h = splits.next().unwrap().parse().unwrap();
+    (l, w, h)
+}
+
+fn paper_req_lwh(l: i32, w: i32, h: i32) -> i32 {
+    let side1 = l * w;
+    let side2 = w * h;
+    let side3 = h * l;
+
+    let surface_area = 2 * side1 + 2 * side2 + 2 * side3;
+    let extra = *[side1, side2, side3].iter().min().unwrap();
+    surface_area + extra
+}
+
+fn paper_req_present_spec(spec: &str) -> i32 {
+    let (l, w, h) = parse_lwh(spec);
+    paper_req_lwh(l, w, h)
+}
+
+fn shortest_distance_around_sides(l: i32, w: i32, h: i32) -> i32 {
+    let mut sides = [l, w, h];
+    sides.sort();
+    (sides[0] * 2 + sides[1] * 2)
+}
+
+fn ribbon_req_lwh(l: i32, w: i32, h: i32) -> i32 {
+    let ribbon_req = shortest_distance_around_sides(l, w, h);
+    let volume = l * w * h;
+    ribbon_req + volume
+}
+
+fn ribbon_req_present_spec(spec: &str) -> i32 {
+    let (l, w, h) = parse_lwh(spec);
+    ribbon_req_lwh(l, w, h)
+}
+
+fn part1(input: &str) -> i32 {
+    input.lines().map(paper_req_present_spec).sum()
+}
+
+fn part2(input: &str) -> i32 {
+    input.lines().map(ribbon_req_present_spec).sum()
+}
+
+aoc::tests! {
+    test1 for part1:
+    "2x3x4" = 58
+    "1x1x10" = 43
+    [1588178]
+    test2 for part2:
+    "2x3x4" = 34
+    "1x1x10" = 14
+    [3783758]
+}
+
+aoc::main!(1 = part1, 2 = part2);

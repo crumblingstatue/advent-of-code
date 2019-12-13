3128d28c5158c32c84a6db8c7680dea57b01c6fb
crumblingstatue <radiantstatue@gmail.com>
2019-12-13 23:42:14 +0100

    Add solution for 2015 day 8

---
+++ src/bin/15d8.rs
@@ -0,0 +1,113 @@
+enum State {
+    /// Interpret characters as normal
+    Normal,
+    /// A backslash escape was detected
+    Escape,
+    /// \x was detected
+    Hex1,
+    /// First digit was parsed
+    Hex2,
+}
+
+fn decode(source: &[u8]) -> Vec<u8> {
+    use State::*;
+    let mut state = Normal;
+    let mut buf = Vec::new();
+    let mut d1: u8 = 0;
+    let mut d2: u8;
+    for &ch in source.into_iter() {
+        match state {
+            Normal => {
+                match ch {
+                    b'\\' => state = Escape,
+                    b'"' => {} // Just ignore quotes. What could possibly go wrong?
+                    _ => buf.push(ch),
+                }
+            }
+            Escape => match ch {
+                c @ b'\\' | c @ b'"' => {
+                    buf.push(c);
+                    state = Normal;
+                }
+                b'x' => {
+                    state = Hex1;
+                }
+                _ => panic!("Invalid escape: {}", ch),
+            },
+            Hex1 => {
+                d1 = ch;
+                state = Hex2;
+            }
+            Hex2 => {
+                d2 = ch;
+                state = Normal;
+                // Lol inefficient
+                buf.push(u8::from_str_radix(&format!("{}{}", d1 as char, d2 as char), 16).unwrap());
+            }
+        }
+    }
+    buf
+}
+
+fn encode(source: &[u8]) -> Vec<u8> {
+    let mut buf = Vec::new();
+
+    // Firstly lol
+    buf.push(b'"');
+    for &b in source.iter() {
+        if b == b'"' || b == b'\\' {
+            buf.push(b'\\');
+        }
+        buf.push(b);
+    }
+    // Finally lol
+    buf.push(b'"');
+    buf
+}
+
+fn part1(input: &str) -> usize {
+    let mut src_len = 0;
+    let mut repr_len = 0;
+    for line in input.lines() {
+        src_len += line.len();
+        let decoded = decode(line.as_bytes());
+        repr_len += decoded.len();
+    }
+    src_len - repr_len
+}
+
+fn part2(input: &str) -> usize {
+    let mut src_len = 0;
+    let mut encoded_len = 0;
+    for line in input.lines() {
+        src_len += line.len();
+        let encoded = encode(line.as_bytes());
+        encoded_len += encoded.len();
+    }
+    encoded_len - src_len
+}
+
+aoc::tests! {
+    test1 for decode:
+    b"\"\"" = b""
+    b"\"abc\"" = b"abc"
+    b"\"aaa\\\"aaa\"" = b"aaa\"aaa"
+    b"\\x27" = b"'"
+    test2 for part1:
+    r#"
+    ""
+    "abc"
+    "aaa\"aaa"
+    "\x27"
+    "# = 12
+    [1350]
+    test3 for encode:
+    br#""""# = br#""\"\"""#
+    br#""abc""# = br#""\"abc\"""#
+    br#""aaa\"aaa""# = br#""\"aaa\\\"aaa\"""#
+    br#""\x27""# = br#""\"\\x27\"""#
+    test4 for part2:
+    [2085]
+}
+
+aoc::main!(1 = part1, 2 = part2);

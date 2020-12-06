1f3dd8923d589e9e15dd57d3e3798edf7cedfee9
crumblingstatue <radiantstatue@gmail.com>
2020-12-06 19:04:54 +0100

    Add 2020 day 6

---
+++ src/bin/20d6.rs
@@ -0,0 +1,85 @@
+fn is_az(b: &u8) -> bool {
+    (b'a'..=b'z').contains(&b)
+}
+
+type Ans = u8;
+
+//                               Group              Form               Ans
+fn group_iter(
+    input: &'_ str,
+) -> impl Iterator<Item = impl Iterator<Item = impl Iterator<Item = Ans> + '_>> {
+    input.split("\n\n").map(form_iter)
+}
+
+//                              Form               Ans
+fn form_iter(input: &'_ str) -> impl Iterator<Item = impl Iterator<Item = Ans> + '_> {
+    input.lines().map(ans_iter)
+}
+
+fn ans_iter(input: &'_ str) -> impl Iterator<Item = Ans> + '_ {
+    input.bytes().filter(is_az)
+}
+
+fn count_unique(iter: impl Iterator<Item = Ans>) -> usize {
+    let mut was = [false; 128];
+    let mut count = 0;
+    for ans in iter {
+        let was = &mut was[ans as usize];
+        if !(*was) {
+            count += 1;
+            *was = true;
+        }
+    }
+    count
+}
+
+//                 Form               Ans
+fn count_all(iter: impl Iterator<Item = impl Iterator<Item = Ans>>) -> usize {
+    let mut counts = [0; 128];
+    let mut forms = 0;
+    for form in iter {
+        for ans in form {
+            counts[ans as usize] += 1;
+        }
+        forms += 1;
+    }
+    counts.iter().filter(|&&count| count == forms).count()
+}
+
+fn part1(input: &str) -> usize {
+    group_iter(input)
+        .map(|group| count_unique(group.flatten()))
+        .sum()
+}
+
+fn part2(input: &str) -> usize {
+    group_iter(input).map(count_all).sum()
+}
+
+#[cfg(test)]
+const TEST_INPUT: &str = "abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b";
+
+aoc::tests! {
+    fn part1:
+    TEST_INPUT => 11
+    => 6683
+    fn part2:
+    TEST_INPUT => 6
+    => 3122
+}
+
+aoc::main!(part1, part2);

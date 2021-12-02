f51afa646e6a0a3849ae8eb7e7a5f184a49b0ecb
crumblingstatue <radiantstatue@gmail.com>
2021-12-02 16:46:25 +0100

    21d2 part 1

---
+++ src/bin/21d2.rs
@@ -0,0 +1,36 @@
+fn commands(input: &str) -> impl Iterator<Item = (&str, u32)> {
+    input.lines().map(|line| {
+        let (fst, sec) = line.split_once(' ').unwrap();
+        (fst, sec.parse().unwrap())
+    })
+}
+
+fn part1(input: &str) -> u32 {
+    let mut hpos = 0;
+    let mut depth = 0;
+    for (dir, amount) in commands(input) {
+        match dir {
+            "forward" => hpos += amount,
+            "down" => depth += amount,
+            "up" => depth -= amount,
+            _ => panic!("Invalid direction: {dir}"),
+        }
+    }
+    hpos * depth
+}
+
+#[cfg(test)]
+const INPUT: &str = "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2";
+
+aoc::tests! {
+    fn part1:
+    INPUT => 150
+    => 1762050
+}
+
+aoc::main!(part1);

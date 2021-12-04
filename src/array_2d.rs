f7a53e59882c0dcf83e8b528ff3214c46213931e
crumblingstatue <radiantstatue@gmail.com>
2021-12-04 07:32:00 +0100

    21d4 part 1

--- src/array_2d.rs
+++ src/array_2d.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 pub struct Array2D<T> {
     buf: Vec<T>,
     width: usize,
@@ -15,6 +17,13 @@ impl<T> Array2D<T> {
             buf: vec![fill; width * height],
         }
     }
+    pub fn from_flat(width: usize, height: usize, iter: impl Iterator<Item = T>) -> Self {
+        Self {
+            width,
+            height,
+            buf: Vec::from_iter(iter),
+        }
+    }
     pub fn get_mut(&mut self, x: usize, y: usize) -> &mut T {
         &mut self.buf[y * self.width + x]
     }
@@ -29,5 +38,33 @@ impl<T> Array2D<T> {
     }
     pub fn flat_iter(&self) -> impl Iterator<Item = &T> {
         self.buf.iter()
+    }
+    pub fn flat_mut(&mut self) -> impl Iterator<Item = &mut T> {
+        self.buf.iter_mut()
+    }
+    pub fn rows(&self) -> impl Iterator<Item = &[T]> {
+        self.buf.chunks_exact(self.width)
+    }
+    pub fn cols(&self) -> impl Iterator<Item = Vec<&T>> {
+        (0..self.width).map(|x| {
+            let mut col = Vec::with_capacity(self.height);
+            for y in 0..self.height {
+                col.push(self.get(x, y));
+            }
+            col
+        })
+    }
+}
+
+impl<T: Debug> Debug for Array2D<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        writeln!(f)?;
+        for y in 0..self.height {
+            for x in 0..self.width {
+                write!(f, "{:?} ", self.get(x, y))?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
     }
 }
---
+++ src/bin/21d4.rs
@@ -0,0 +1,155 @@
+use std::{fmt::Debug, num::ParseIntError, str::FromStr};
+
+use aoc::array_2d::Array2D;
+
+#[derive(PartialEq)]
+struct Markable {
+    num: u32,
+    mark: bool,
+}
+
+impl Debug for Markable {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.num)?;
+        if self.mark {
+            write!(f, "x")?;
+        }
+        Ok(())
+    }
+}
+
+impl FromStr for Markable {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self {
+            num: s.parse()?,
+            mark: false,
+        })
+    }
+}
+
+#[derive(Debug)]
+struct Board {
+    array: Array2D<Markable>,
+}
+
+impl Board {
+    fn from_str(input: &str) -> Self {
+        let num_tokens = input.split_whitespace().map(|tok| tok.parse().unwrap());
+        Self {
+            array: Array2D::from_flat(5, 5, num_tokens),
+        }
+    }
+    fn mark_num(&mut self, num: u32) {
+        for field in self.array.flat_mut() {
+            if field.num == num {
+                field.mark = true;
+            }
+        }
+    }
+    fn winning_vector(&self) -> Option<[u32; 5]> {
+        for row in self.array.rows() {
+            if row.iter().all(|field| field.mark) {
+                return Some([row[0].num, row[1].num, row[2].num, row[3].num, row[4].num]);
+            }
+        }
+        for col in self.array.cols() {
+            if col.iter().all(|field| field.mark) {
+                return Some([col[0].num, col[1].num, col[2].num, col[3].num, col[4].num]);
+            }
+        }
+        None
+    }
+    fn unmarked(&self) -> impl Iterator<Item = u32> + '_ {
+        self.array
+            .flat_iter()
+            .filter_map(|m| if !m.mark { Some(m.num) } else { None })
+    }
+}
+
+#[test]
+fn test_board_from_str() {
+    let nums = "22 13 17 11  0
+     8   2 23  4 24
+    21   9 14 16  7
+     6  10  3 18  5
+     1  12 20 15 19";
+    let board = Board::from_str(nums);
+    assert_eq!(
+        board.array.get(0, 0),
+        &Markable {
+            num: 22,
+            mark: false
+        }
+    );
+    assert_eq!(
+        board.array.get(1, 1),
+        &Markable {
+            num: 2,
+            mark: false
+        }
+    );
+    assert_eq!(
+        board.array.get(4, 4),
+        &Markable {
+            num: 19,
+            mark: false
+        }
+    );
+}
+
+fn draw_and_boards(input: &str) -> (Vec<u32>, Vec<Board>) {
+    let mut segments = input.split("\n\n");
+    let draw: Vec<u32> = segments
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|tok| tok.parse().unwrap())
+        .collect();
+    let boards: Vec<Board> = segments.map(Board::from_str).collect();
+    (draw, boards)
+}
+
+fn part1(input: &str) -> u32 {
+    let (draw, mut boards) = draw_and_boards(input);
+    for draw_num in draw {
+        for board in &mut boards {
+            board.mark_num(draw_num);
+            if let Some(_vec) = board.winning_vector() {
+                return board.unmarked().sum::<u32>() * draw_num;
+            }
+        }
+    }
+    panic!("No winning board")
+}
+
+#[cfg(test)]
+const TEST_INPUT: &str = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7";
+
+aoc::tests!(
+    fn part1:
+    TEST_INPUT => 4512;
+    => 45031;
+    !=> 11067;
+);
+
+aoc::main!(part1);

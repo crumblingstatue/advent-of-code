f53b2a385b1151fd924b920928cee14ef69013dd
crumblingstatue <radiantstatue@gmail.com>
2021-12-06 18:31:26 +0100

    Methodify

--- src/bin/21d6.rs
+++ src/bin/21d6.rs
@@ -12,56 +12,55 @@ impl Debug for School {
     }
 }
 
-fn school_str(input: &str) -> School {
-    school(
-        input
-            .trim()
-            .split(',')
-            .map(|tok| tok.parse::<u64>().unwrap()),
-    )
-}
-
-fn school(fishes: impl Iterator<Item = u64>) -> School {
-    let mut school = [0; 10];
-    for fish in fishes {
-        school[fish as usize] += 1;
+impl School {
+    fn from_fishes_iter(fishes: impl Iterator<Item = u64>) -> School {
+        let mut school = [0; 10];
+        for fish in fishes {
+            school[fish as usize] += 1;
+        }
+        School(school)
     }
-    School(school)
-}
-
-fn simulate(school: &mut School, cycles: u32) {
-    for _ in 0..cycles {
-        sim_cycle(school);
+    fn from_str(input: &str) -> School {
+        Self::from_fishes_iter(
+            input
+                .trim()
+                .split(',')
+                .map(|tok| tok.parse::<u64>().unwrap()),
+        )
     }
-}
-
-fn sim_cycle(School(school): &mut School) {
-    let mut school_8_add = 0;
-    // Each day, a 0 becomes a 6 and adds a new 8 to the end of the list,
-    if school[0] > 0 {
-        school[7] += school[0];
-        school_8_add = school[0];
-        school[0] = 0;
+    fn simulate_for(&mut self, cycles: u32) {
+        for _ in 0..cycles {
+            self.do_sim_cycle();
+        }
     }
-    // while each other number decreases by 1 if it was present at the start of the day.
-    school.copy_within(1..=9, 0);
-    school[8] += school_8_add;
-}
-
-fn count(School(school): &School) -> u64 {
-    school.iter().sum()
+    fn do_sim_cycle(&mut self) {
+        let school = &mut self.0;
+        let mut school_8_add = 0;
+        // Each day, a 0 becomes a 6 and adds a new 8 to the end of the list,
+        if school[0] > 0 {
+            school[7] += school[0];
+            school_8_add = school[0];
+            school[0] = 0;
+        }
+        // while each other number decreases by 1 if it was present at the start of the day.
+        school.copy_within(1..=9, 0);
+        school[8] += school_8_add;
+    }
+    fn count(&self) -> u64 {
+        self.0.iter().sum()
+    }
 }
 
 fn part1(input: &str) -> u64 {
-    let mut school = school_str(input);
-    simulate(&mut school, 80);
-    count(&school)
+    let mut school = School::from_str(input);
+    school.simulate_for(80);
+    school.count()
 }
 
 fn part2(input: &str) -> u64 {
-    let mut school = school_str(input);
-    simulate(&mut school, 256);
-    count(&school)
+    let mut school = School::from_str(input);
+    school.simulate_for(256);
+    school.count()
 }
 
 #[cfg(test)]
@@ -96,15 +95,15 @@ fn cycle_test() {
             6, 0, 6, 4, 5, 6, 0, 1, 1, 2, 6, 0, 1, 1, 1, 2, 2, 3, 3, 4, 6, 7, 8, 8, 8, 8,
         ],
     ];
-    let mut test_school = school(test_days[0].iter().cloned());
+    let mut test_school = School::from_fishes_iter(test_days[0].iter().cloned());
     for (i, day) in test_days.into_iter().enumerate() {
         if i == 0 {
             eprint!("Initial state: ");
         } else {
             eprint!("After {} days: ", i);
         }
-        assert_eq!(test_school, school(day.iter().cloned()));
-        sim_cycle(&mut test_school);
+        assert_eq!(test_school, School::from_fishes_iter(day.iter().cloned()));
+        test_school.do_sim_cycle();
         eprintln!("[OK]");
     }
 }

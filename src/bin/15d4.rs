f84d4893dc04e4e24e30c5cb2778367c8f269434
crumblingstatue <radiantstatue@gmail.com>
2021-12-07 23:09:19 +0100

    Improve efficiency of 15d4 a bit

--- src/bin/15d4.rs
+++ src/bin/15d4.rs
@@ -1,8 +1,12 @@
+use std::fmt::Write;
+
 fn part1(input: &str) -> i32 {
     let mut num = 0;
+    let mut hash_input = String::new();
     loop {
-        let hash_input = format!("{}{}", input, num);
+        let _ = write!(&mut hash_input, "{}{}", input, num);
         let digest = md5::compute(hash_input.as_bytes());
+        hash_input.clear();
         if digest[0] == 0 && digest[1] == 0 && digest[2] < 16 {
             return num;
         }
@@ -12,9 +16,11 @@ fn part1(input: &str) -> i32 {
 
 fn part2(input: &str) -> i32 {
     let mut num = 0;
+    let mut hash_input = String::new();
     loop {
-        let hash_input = format!("{}{}", input, num);
+        let _ = write!(&mut hash_input, "{}{}", input, num);
         let digest = md5::compute(hash_input.as_bytes());
+        hash_input.clear();
         if digest[0] == 0 && digest[1] == 0 && digest[2] == 0 {
             return num;
         }

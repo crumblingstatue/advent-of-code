372e1e6369a0e552e4e3b224c719878ca222558a
crumblingstatue <radiantstatue@gmail.com>
2022-12-06 18:43:40 +0100

    2022, day 6, part 2

--- src/bin/22d6.rs
+++ src/bin/22d6.rs
@@ -1,8 +1,8 @@
 use std::collections::HashSet;
 
-fn sequence_has_4_unique(seq: &[u8]) -> bool {
+fn sequence_has_n_unique<const N: usize>(seq: &[u8]) -> bool {
     let mut encountered = HashSet::new();
-    for &ch in seq.iter().take(4.min(seq.len())) {
+    for &ch in seq.iter().take(N.min(seq.len())) {
         if encountered.contains(&ch) {
             return false;
         }
@@ -11,34 +11,40 @@ fn sequence_has_4_unique(seq: &[u8]) -> bool {
     true
 }
 
-fn find_marker(input: &str) -> usize {
+fn find_marker<const N: usize>(input: &str) -> usize {
     let input = input.as_bytes();
 
     for i in 0..input.len() {
-        if sequence_has_4_unique(&input[i..]) {
-            return i + 4;
+        if sequence_has_n_unique::<N>(&input[i..]) {
+            return i + N;
         }
     }
     panic!("Couldn't find marker");
 }
 
 fn part1(input: &str) -> usize {
-    find_marker(input)
+    find_marker::<4>(input)
+}
+
+fn part2(input: &str) -> usize {
+    find_marker::<14>(input)
 }
 
 aoc::tests! {
-fn sequence_has_4_unique:
-    b"mjqj" => false;
-    b"jpqm" => true;
-
-fn find_marker:
+fn part1:
     "mjqjpqmgbljsphdztnvjfqwrcgsmlb" => 7;
     "bvwbjplbgvbhsrlpgdmjqwftvncz" => 5;
     "nppdvjthqldpwncqszvftbrmjlhg" => 6;
     "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" => 10;
     "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" => 11;
-fn part1:
     in => 1766;
+fn part2:
+    "mjqjpqmgbljsphdztnvjfqwrcgsmlb" => 19;
+    "bvwbjplbgvbhsrlpgdmjqwftvncz" => 23;
+    "nppdvjthqldpwncqszvftbrmjlhg" => 23;
+    "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" => 29;
+    "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" => 26;
+    in => 2383;
 }
 
-aoc::main!(part1);
+aoc::main!(part1, part2);
